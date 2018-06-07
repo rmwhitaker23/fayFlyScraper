018cee9ee2d8bea649efbceac3cd243368ac257c
Ryne Whitaker <40004506+rmwhitaker23@users.noreply.github.com>
2018-06-07 13:03:15 -0500

    Added collection/output of day of week
    FINALLY! 
    fayFlyScraper can now collect the day of week of each event and output it with each event.  Events will now be searchable by date.

--- master.py
+++ master.py
@@ -21,59 +21,47 @@ soup = BeautifulSoup(page.content, 'html.parser')
 # Write table headers to top of output file to be used when importing data
 # Use system check to prevent enable compatibility w/ Windows and Linux
 if sys.platform.startswith('linux'):
-	output.write("Title; Time; Venue; Category\n")
+  output.write("Title; Day of Week; Time; Venue; Category\n")
 else:
-	output.write("Title-_- Time-_- Venue-_- Category\n")
+  output.write("Title-_- Day -_- Time-_- Venue-_- Category\n")
 
-# Find number of instances of times on list
-numTimes = len(soup.find_all(class_="event_time"))
+# Find the number of tables on the page (for looping)
+numTables = len(soup.find_all('table'))
 
-# Fetch data for the amount of times "event_time" appears on page (used as number of events)
-for y in range(numTimes):
-	#Get title, time, venue, and category of event y
-	title = soup.find_all('h4')[y].get_text()
-	time = soup.find_all(class_="event_time")[y].get_text()
-	venue = soup.find_all(class_="event_venue")[y].get_text()
-	category = soup.find_all(class_="event_category")[y].get_text()
-	# Print event info with new line to separate them
-	# Don't need this, but nice to see when developing
-	print(title)
-	print(time)
-	print(venue)
-	print(category)
-	print("\n")
-	# Use platform check to prevent enable compatibility w/ Windows and Linux
-	if sys.platform.startswith('linux'):
-		# Output to text file using .encode('utf-8') on Linux (repl.it)
-		output.write(title.encode('utf-8') + "; ")
-		output.write(time.encode('utf-8') + "; ")
-		output.write(venue.encode('utf-8') + "; ")
-		output.write(category.encode('utf-8') + "\n")
-	else:
-		# Output without .encode, and change | to -_- or similar on Windows
-    # see if ; works on Windows... could get rid of platform check in that case
-		output.write(title + "-_- ")
-		output.write(time + "-_- ")
-		output.write(venue + "-_- ")
-		output.write(category + "\n")
-''' #For testing on first event in calendar
-x=1
-title = soup.find_all('h4')[x].get_text()
-time = soup.find_all(class_="event_time")[x].get_text()
-day = soup.find_all(class_="event_day")[x].get_text()
-venue = soup.find_all(class_="event_venue")[x].get_text()
-category = soup.find_all(class_="event_category")[x].get_text()
-print(title)
-print(day)
-print(time)
-print(venue)
-print(category)
-print("\n") '''
-''' # find number of days
-numChildren = len(soup.find_all('h2'))
-print numChildren '''
-
-# could do count of h2 children (events) and do separate loop to loop for that many times, then update the day
+for z in range(numTables):
+  # Store day of week for table z
+  dayOfWeek = soup.find_all(class_="event_day")[z].get_text()
+  # Store table of events for table z
+  dayTable = soup.find_all('table')[z]
+  #Get title, time, venue, and category of event y
+  numTimes = len(dayTable.find_all(class_="event_time"))
+  for y in range(numTimes):
+    title = dayTable.find_all('h4')[y].get_text()
+    time = dayTable.find_all(class_="event_time")[y].get_text()
+    venue = dayTable.find_all(class_="event_venue")[y].get_text()
+    category = dayTable.find_all(class_="event_category")[y].get_text()
+    print(title)
+    print(dayOfWeek)
+    print(time)
+    print(venue)
+    print(category)
+    print("\n")
+  	# Use platform check to prevent enable compatibility w/ Windows and Linux
+    if sys.platform.startswith('linux'):
+      # Output to text file using .encode('utf-8') on Linux (repl.it)
+      output.write(title.encode('utf-8') + "; ")
+      output.write(dayOfWeek.encode('utf-8') + "; ")
+      output.write(time.encode('utf-8') + "; ")
+      output.write(venue.encode('utf-8') + "; ")
+      output.write(category.encode('utf-8') + "\n")
+    else:
+      # Output without .encode, and change | to -_- or similar on Windows
+      # see if ; works on Windows... could get rid of platform check in that case
+      output.write(title + "-_- ")
+      output.write(dayOfWeek + "-_- ")
+      output.write(time + "-_- ")
+      output.write(venue + "-_- ")
+      output.write(category + "\n")
 
 # Close the output file
 output.close()
